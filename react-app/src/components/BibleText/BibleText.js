b6b68ba7266d2198b2991cddaa1fc2cb8fc52ca4
Serghei Mineev <mineevserghei@gmail.com>
2023-05-17 14:05:38 -0400

    add menu component to BibleText

--- react-app/src/components/BibleText/BibleText.js
+++ react-app/src/components/BibleText/BibleText.js
@@ -1,12 +1,15 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { getAllBooksThunk } from "../../store/bible";
-
+import './BibleText.css'
 
 export default function BibleText() {
 
     const dispatch = useDispatch();
     const booksObj = useSelector(state => state.bible)
+
+    const [selectedBook, setSelectedBook] = useState('Genesis');
+    const [booksMenuOpen, setBooksMenuOpen] = useState(false);
 
     const books = Object.values(booksObj)
 
@@ -25,8 +28,21 @@ export default function BibleText() {
 
     return (
         <div>
-            <h1>BibleText</h1>
-            {books.map(book => <p key={book.id}>{book.name}</p>)}
+            <div className="book-top">
+                <div className="book-menu">
+                    <h2
+                        onClick={() => setBooksMenuOpen(!booksMenuOpen)}
+                        className="selected-book-title">{booksObj[selectedBook].name}</h2>
+
+                    {booksMenuOpen && <div className="select-book">
+                        {books.map(book => <p
+                            className="book-selection"
+                            value={book.name}
+                            key={book.id}>{book.name}</p>)}
+                    </div>}
+                </div>
+            </div>
+
         </div>
     );
 }

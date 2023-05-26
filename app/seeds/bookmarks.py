72d6a7d6c655b782aeaf29763cbe46782e601686
Serghei Mineev <mineevserghei@gmail.com>
2023-05-26 18:49:31 -0400

    Add seeds for bookmarks

---
+++ app/seeds/bookmarks.py
@@ -0,0 +1,31 @@
+from app.models import db, Bookmark, environment, SCHEMA
+from sqlalchemy.sql import text
+
+def seed_bookmarks():
+
+    bookmark1 = Bookmark(
+        user_id=1,
+        verse_id=1000,
+        tag="verse #1000!",
+        color="#3a98b9"
+    )
+
+    bookmark2 = Bookmark(
+        user_id=1,
+        verse_id=2000,
+        tag="verse #2000!",
+        color="#ffd183"
+    )
+
+    db.session.add(bookmark1)
+    db.session.add(bookmark2)
+
+    db.session.commit()
+
+def undo_bookmarks():
+    if environment == "production":
+        db.session.execute(f"TRUNCATE table {SCHEMA}.bookmarks RESTART IDENTITY CASCADE;")
+    else:
+        db.session.execute(text("DELETE FROM bookmarks"))
+
+    db.session.commit()

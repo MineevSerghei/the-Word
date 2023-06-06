e15fab97466d0022c15f9dcb1ccf9792d1a60a5a
Serghei Mineev <mineevserghei@gmail.com>
2023-06-06 14:18:02 -0400

    Initialize BookmarksOptions component

---
+++ react-app/src/components/BookmarksOptions/BookmarksOptions.js
@@ -0,0 +1,21 @@
+import "./BookmarksOptions.css"
+
+export default function BookmarksOptions({ }) {
+
+    return (
+        <h2>BookmarksOptions</h2>
+        // <div style={{ left: `${x - 100}px`, top: `${y - 200}px` }} className="pop-up-container">
+        //     <i className="fa-solid fa-xmark note-close" onClick={() => setPopUpOpen(0)}></i>
+        //     <p className="verse-ref-note">{book.name} {chapter.number}:{verse.number}</p>
+
+        //     <div className="popup-buttons">
+
+        //         <button className="popup-button" disabled>Send</button>
+        //         <button className="popup-button" disabled>Bookmark</button>
+        //         <button className="popup-button" disabled={!user} onClick={openNoteField}>Note</button>
+        //         <button className="popup-button" onClick={copy}> {copied ? "Copied ✅" : "Copy"}</button>
+        //     </div>
+
+        // </div >
+    )
+}

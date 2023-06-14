7e4f50e26914f007b149938ace0c89c52090e71c
Serghei Mineev <mineevserghei@gmail.com>
2023-06-14 11:12:44 -0400

    write out AWS helpers and add form validation for images

--- app/forms/plan_form.py
+++ app/forms/plan_form.py
@@ -1,9 +1,11 @@
 
 from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField
+from flask_wtf.file import FileField, FileAllowed
 from wtforms.validators import DataRequired, Length, NumberRange
 
 class PlanForm(FlaskForm):
     name = StringField('name', validators=[DataRequired(), Length(max=70, message='The plan name has to be 70 characters or less.')])
     description = StringField('description', validators=[DataRequired(), Length(max=700, message='The plan description has to be 700 characters or less.')])
     duration = IntegerField('duration', validators=[DataRequired(), NumberRange(max=365, min=3, message='The plan duration cannot be longer than 1 year (365 days) or shorter than 3 days.')])
+    image = FileField("Image", validators=[FileAllowed(["png", "jpg", "jpeg"])])
